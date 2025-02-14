8b1e6ef81ea3a154f6546b06f89b161c6786ad33
You Young June <youngjune@planorban.com>
2025-02-14 00:34:16 -0500

    added youtube

--- src/components/Computer.tsx
+++ src/components/Computer.tsx
@@ -7,14 +7,28 @@ import { Input } from "./ui/input";
 
 export const Computer = () => {
   const [loginForm, setLoginForm] = useState<boolean>();
+  const [preview, setPreview] = useState<boolean>(false);
 
   return (
     <Card className="flex justify-center items-center bg-blue-600 text-white border-8 border-stone-700 min-h-screen lg:min-h-0 lg:h-[35rem]">
-      {!loginForm ? (
-        <Button onClick={() => setLoginForm(!loginForm)}>
-          {/* TODO: replace with logo */}
-          Replace with logo
-        </Button>
+      {preview ? (
+        <iframe
+          // width="560"
+          // height="315"
+          src="https://www.youtube.com/embed/videoseries?si=s1UK-ua6N_tg-ejc&amp;list=PLmqHrTntDj-lSOZhejmz_k_2CzYv87yrt"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="w-full h-full"
+        ></iframe>
+      ) : !loginForm ? (
+        <img
+          onClick={() => setLoginForm(!loginForm)}
+          src="https://m2my1rq4tt.ufs.sh/f/83acf69d-8644-4b12-a4ee-eb3508732d7b-xbioow.png"
+          className="h-52 hover:cursor-pointer"
+        />
       ) : (
         <div className="flex flex-col space-y-2">
           <p className="font-bold flex justify-center">Welcome Back!</p>
@@ -23,10 +37,20 @@ export const Computer = () => {
           <Button className="w-full">Login</Button>
         </div>
       )}
-      <div className="absolute mt-72">
-        {/* TODO: need to make this click through to youtube playlist and singles */}
-        <Button variant="link">Preview</Button>
-      </div>
+      {preview ? (
+        <div className="absolute mt-72">
+          <Button variant="destructive" onClick={() => setPreview(false)}>
+            Return
+          </Button>
+        </div>
+      ) : (
+        <div className="absolute mt-72">
+          {/* TODO: need to make this click through to youtube playlist and singles */}
+          <Button variant="link" onClick={() => setPreview(true)}>
+            Preview
+          </Button>
+        </div>
+      )}
     </Card>
   );
 };
--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -7,10 +7,23 @@ import { Input } from "./ui/input";
 
 export const Phone = () => {
   const [signUpForm, setSignUpForm] = useState<boolean>();
+  const [preview, setPreview] = useState<boolean>(false);
 
   return (
     <Card className="flex justify-center items-center h-96 bg-blue-600 text-white border-8 border-stone-700">
-      {signUpForm ? (
+      {preview ? (
+        <iframe
+          // width="560"
+          // height="315"
+          src="https://www.youtube.com/embed/videoseries?si=VoK63HYORtoH4TSL&amp;list=PLmqHrTntDj-ksa6BcleEgFZP_wUeLRzCL"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="w-full h-full"
+        ></iframe>
+      ) : signUpForm ? (
         <div className="flex flex-col items-center space-y-2">
           <p className="font-bold">Who Is You?</p>
           <Input placeholder="Email" />
@@ -25,9 +38,19 @@ export const Phone = () => {
           Create an account for more content!
         </Button>
       )}
-      <div className="absolute mt-72">
-        <Button variant="link">Preview</Button>
-      </div>
+      {preview ? (
+        <div className="absolute mt-72">
+          <Button variant="destructive" onClick={() => setPreview(false)}>
+            Return
+          </Button>
+        </div>
+      ) : (
+        <div className="absolute mt-72">
+          <Button variant="link" onClick={() => setPreview(true)}>
+            Preview
+          </Button>
+        </div>
+      )}
     </Card>
   );
 };
